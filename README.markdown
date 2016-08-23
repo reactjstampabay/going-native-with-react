a9120ced9c8b04472d9a9b6fe019f1f0722e698c
Eric Nograles <ENograles@352inc.com>
2016-08-22 20:35:26 -0400

    updates markdown and title slide

--- README.markdown
+++ README.markdown
@@ -1,21 +1,5 @@
-# Spectacle Boilerplate
+# ReactJS Tampa Bay
 
-## Reference
+## Going Native With React (August 31, 2016)
 
-The Spectacle core API is available at [https://github.com/FormidableLabs/spectacle/blob/master/README.markdown](https://github.com/FormidableLabs/spectacle/blob/master/README.markdown).
-
-## Development
-
-After downloading the boilerplate, your first order of business is to open terminal and run `npm install`
-
-Next, run `rm -R .git` to remove the existing version control.
-
-Then, to start up the local server, run `npm start`
-
-Open a browser and hit [http://localhost:3000](http://localhost:3000), and we are ready to roll
-
-## Build & Deployment
-
-Building the dist version of the project is as easy as running `npm run build`
-
-If you want to deploy the slideshow to surge, run `npm run deploy`
+[ ![Codeship Status for reactjstampabay/going-native-with-react](https://codeship.com/projects/e639d940-4af0-0134-ea43-5a647d62e392/status?branch=master)](https://codeship.com/projects/169796)
--- presentation/index.js
+++ presentation/index.js
@@ -45,7 +45,7 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#61dafb"
 });
 
 export default class Presentation extends React.Component {
@@ -55,18 +55,8 @@ export default class Presentation extends React.Component {
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
           <Slide transition={["zoom"]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+              Going Native With React
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
           <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
             <Image src={images.kat} margin="0px auto 40px" height="293px"/>
